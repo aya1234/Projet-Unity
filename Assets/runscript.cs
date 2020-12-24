bc799d8b23ec77f5997cc366c803cee00a687bd6
aya <lazzemaya1@gmail.com>
2020-12-24 23:26:24 +0100

    first commit

---
+++ Assets/runscript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class runscript : MonoBehaviour
+{
+    public Rigidbody2D myrigidBody;    
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }       
+
+    private void Move()     
+    {        
+        var moveX = Input.GetAxisRaw("Horizontal");        
+        myrigidBody.velocity = moveX*speed*Vector2.right;     
+    }
+
+}
